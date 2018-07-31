6268950f24cc3ef9f5b770b8fbd20ddbd135d65f
Sarah Marsh <sarah.marsh.engineer@gmail.com>
2018-07-31 16:49:11 -0500

    Add logic to assign light addresses

--- DALIDriver.cpp
+++ DALIDriver.cpp
@@ -19,14 +19,112 @@
 DALIDriver::DALIDriver(PinName out_pin, PinName in_pin, int baud, bool idle_state)
     : encoder(out_pin, in_pin, baud, idle_state)
 {
+}
 
+void DALIDriver::send_command(uint8_t address, uint8_t opcode) 
+{
+    encoder.send((address << 8) | opcode);
+}
 
+bool DALIDriver::check_response(uint8_t expected) 
+{
+    uint8_t response = encoder.recv();
+    return (response == expected);
+}
 
+int DALIDriver::getIndexOfLogicalUnit(uint8_t addr)
+{
+    send_command(DTR1, 0x00);
+    send_command(DTR0, 0x1A);
+    send_command(READ_MEM_LOC, (addr << 1) + 1);
+    return encoder.recv();
+} 
+
+void DALIDriver::set_search_address(uint32_t val) 
+{
+    send_command(SEARCHADDRH, val >> 16);
+    send_command(SEARCHADDRM, (val >> 8) & (0x00FF));
+    send_command(SEARCHADDRL, val & 0x0000FF);
 }
 
 bool DALIDriver::init() 
 {
-    encoder.send(0x8000);
-    int response = encoder.recv();
-    return (response == 32768); 
+    // TODO: does this need to happen every time controller boots?
+    num_logical_units = assign_addresses();
+    return (num_logical_units != 0);
+}
+    
+// Return number of logical units on the bus
+int DALIDriver::assign_addresses() 
+{
+    int searchCompleted = false;
+    int numAssignedShortAddresses = 0;
+    int assignedAddresses[63] = {false}; 
+    int highestAssigned = -1;
+    send_command(DTR0, 0xFF);
+    send_command(INITIALISE, 0x00);
+    send_command(RANDOMISE, 0x00);
+    wait_ms(100);
+    
+    while(true) {
+        // Set the search address to the highest range
+        set_search_address(0xFFFFFF);
+        // Compare logical units search address to global search address
+        send_command(COMPARE, 0x00);
+        // Check if any device responds yes
+        bool yes = check_response(YES);       
+        // If no devices are unassigned, we are done
+        if (!yes) {
+            return numAssignedShortAddresses;
+        }
+        if(numAssignedShortAddresses < 63) {
+            uint32_t searchAddr = 0xFFFFFF;
+            for(int i = 23; i>=0; i--) {
+                uint32_t mask = 1 << i;
+                searchAddr = searchAddr & (~mask);
+                set_search_address(searchAddr);
+                send_command(COMPARE, 0x00);
+                bool yes = check_response(YES);
+                if(!yes) {
+                    //No unit here, revert the mask
+                    searchAddr = searchAddr | mask;
+                }
+                // If yes, then we found at least one device
+            }
+            set_search_address(searchAddr);
+            send_command(COMPARE, 0x00);
+            bool yes = check_response(YES);
+            if (yes) {
+                // We found a unit, let's program the short address with its index
+                // Give it a temporary short address
+                send_command(PROGRAM_SHORT_ADDR, (63 << 1) + 1);
+                // Find the index
+                int index = getIndexOfLogicalUnit(63);
+                if (index < 63) {
+                    if (assignedAddresses[index] == true) {
+                        // Duplicate addr?
+                    }   
+                    else {
+                        // Program index as short address
+                        send_command(PROGRAM_SHORT_ADDR, (index << 1) + 1);
+                        // Tell unit to withdraw
+                        send_command(WITHDRAW, 0x00);
+                        numAssignedShortAddresses++;
+                        assignedAddresses[index] = true;
+                        if(index > highestAssigned) 
+                            highestAssigned = index;
+                    }
+                }
+                else {
+                    // expected < 63 ?
+                }
+            }
+            else {
+                // No device found
+            }
+        }
+        // Refresh initialization state
+        send_command(INITIALISE, 0x00);
+    }
+
 }
--- DALIDriver.h
+++ DALIDriver.h
@@ -20,13 +20,95 @@
 #include "mbed.h"
 #include "manchester/encoder.h"
 
+// Special commands that do not address a specific device
+// These values will be used as address byte in DALI command
+enum SpecialCommandOpAddr {
+    SEARCHADDRH = 0xB1,
+    SEARCHADDRM = 0xB3,
+    SEARCHADDRL = 0xB5,
+    DTR0 = 0xA3,
+    DTR1 = 0xC3,
+    DTR2 = 0xC5,
+    INITIALISE = 0xA5,
+    RANDOMISE = 0xA7,
+    PROGRAM_SHORT_ADDR = 0xB7,
+    COMPARE = 0xA9,
+    WITHDRAW = 0xAB
+};
+
+// Command op codes 
+enum CommandOpCodes {
+    READ_MEM_LOC = 0xC5
+};
+
+#define YES 0xFF
+
 class DALIDriver {
 public:
+    /** Constructor DALIDriver
+     *
+     *  @param out_pin      Output pin for the DALI commands
+     *  @param in_pin       Input pin for responses from DALI slaves
+     *  @param baud         Signal baud rate
+     *  @param idle_state   The default state of the line (high for DALI) 
+     */
     DALIDriver(PinName out_pin, PinName in_pin, int baud = 1200, bool idle_state = 1);
+
+    /** Initialise the driver 
+    *
+    *   @returns    true if successful 
+    *       
+    */
     bool init();
 
 private:
+
+    /** Assign addresses to the logical units on the bus 
+    *
+    *   @returns    The number of logical units found on bus
+    *
+    *   NOTE: This process is copied from page 82 of iec62386-102
+    */ 
+    int assign_addresses();
+
+    /** Set the controller search address 
+    * This address will be used in search commands to determine what 
+    * control units this address or a numerically lower address
+    *
+    *   @param val    Search address valued (only lower 24 bits are used)
+    *
+    */ 
+    void set_search_address(uint32_t val);
+
+    /** Send a command on the bus 
+    *
+    *   @param address     The address byte for command
+    *   @param opcode      The opcode byte 
+    *
+    */ 
+    void send_command(uint8_t address, uint8_t opcode);
+
+    
+    /** Check the response from the bus 
+    *
+    *   @param expected    Expected response from the bus
+    *   @returns           
+    *       True if response matches expected response
+    *
+    */ 
+    bool check_response(uint8_t expected);
+
+    /** Get the index of a control unit
+    *
+    *   @param addr     The address of the device
+    *   @returns        The index of the device
+    */
+    int getIndexOfLogicalUnit(uint8_t addr);
+
+    // The encoder for the bus signals
     ManchesterEncoder encoder;
+    // The number of logical units on the bus
+    int num_logical_units;
 };
 
 #endif
